3492174e9c2a8bc45003977462bd67a2b64468d5
heyipomoea <heyipomoea@gmail.com>
2023-10-12 16:26:12 +0800

    Update 第一人稱玩家設定

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,4 +1,6 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -46,10 +48,28 @@ namespace Heyipomoea
         #endregion
 
 
+        private string namePlayer;
+        private string nameCreateRoom;
+        private string nameJoinRoom;
+        /// <summary>
+        /// 文字房間名稱
+        /// </summary>
+        private TextMeshProUGUI textRoomName;
+        /// <summary>
+        /// 文字房間人數
+        /// </summary>
+        private TextMeshProUGUI textRoomPersonCount;
+        /// <summary>
+        /// 按鈕開始遊戲
+        /// </summary>
+        private Button btnStartGame;
+
         private void Awake()
         {
             GetUIObject();
             PhotonNetwork.ConnectUsingSettings();
+            GetInputFieldData();
+            ButtonClickSetting();
         }
 
         /// <summary>
@@ -65,6 +85,71 @@ namespace Heyipomoea
             btnCreateRoom = GameObject.Find("按鈕創建房間").GetComponent<Button>();
             btnJoinRoom = GameObject.Find("按紐加入房間").GetComponent<Button>();
             btnJoinRandomRoom = GameObject.Find("按紐加入隨機房間").GetComponent<Button>();
+
+            textRoomName = GameObject.Find("文字房間名稱").GetComponent<TextMeshProUGUI>();
+            textRoomPersonCount = GameObject.Find("文字房間人數").GetComponent<TextMeshProUGUI>();
+            btnStartGame = GameObject.Find("按紐開始遊戲").GetComponent<Button>();
+        }
+
+
+        /// <summary>
+        /// 取得輸入欄位資料
+        /// </summary>
+        private void GetInputFieldData()
+        {
+            inputFieldName.onEndEdit.AddListener((input) => namePlayer = input);
+            inputFieldCreateRoomName.onEndEdit.AddListener((input) => nameCreateRoom = input);
+            inputFieldJoinRoomName.onEndEdit.AddListener((input) => nameJoinRoom = input);
+        }
+
+        /// <summary>
+        /// 設定按鈕點擊
+        /// </summary>
+        private void ButtonClickSetting()
+        {
+            btnCreateRoom.onClick.AddListener(BtnCreateRoom);
+            btnJoinRoom.onClick.AddListener(BtnJoinRoom);
+            btnJoinRandomRoom.onClick.AddListener(BtnJoinRandomRoom);
+
+            btnStartGame.onClick.AddListener(() => photonView.RPC("RPCLoadGameScene", RpcTarget.All));
+        }
+
+        private void BtnCreateRoom()
+        {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 20;
+            PhotonNetwork.CreateRoom(nameCreateRoom, roomOptions);
+            btnStartGame.interactable = true;
+        }
+
+        private void BtnJoinRoom()
+        {
+            PhotonNetwork.JoinRoom(nameJoinRoom);
+        }
+
+        private void BtnJoinRandomRoom()
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        /// <summary>
+        /// 更新房間介面
+        /// </summary>
+        private void UpdateRoomUI()
+        {
+            groupRoom.alpha = 1;
+            groupRoom.interactable = true;
+            groupRoom.blocksRaycasts = true;
+
+            textRoomName.text = $"房間名稱: {PhotonNetwork.CurrentRoom.Name}"; 
+            textRoomPersonCount.text = $"遊戲人數: {PhotonNetwork.CurrentRoom.PlayerCount} / {PhotonNetwork.CurrentRoom.MaxPlayers}";
+        }
+
+        [PunRPC]
+        private void RPCLoadGameScene()
+        {
+            Debug.LogError("開始遊戲");
+            PhotonNetwork.LoadLevel("遊戲場景");
         }
 
         public override void OnConnectedToMaster()
@@ -73,8 +158,26 @@ namespace Heyipomoea
             print("連線伺服器成功!");
             groupLobby.interactable = true;
         }
+
+        public override void OnCreatedRoom()
+        {
+            base.OnCreatedRoom();
+            print("創建房間成功!");
+            UpdateRoomUI();
+        }
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+            print("加入房間成功!");
+            UpdateRoomUI();
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+            UpdateRoomUI();
+        }
     }
-
-
 }
 
---
+++ Assets/Scripts/SpawnPlayerSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 生成玩家系統
+    /// </summary>
+    public class SpawnPlayerSystem : MonoBehaviour
+    {
+
+    }
+}
+
