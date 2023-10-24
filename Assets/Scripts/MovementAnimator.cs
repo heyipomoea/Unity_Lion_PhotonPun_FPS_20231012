8607251197e675c53dddf2c46f8f16cba77823a0
heyipomoea <heyipomoea@gmail.com>
2023-10-24 12:02:20 +0800

    Update AnimationRigg and 綁定IK

---
+++ Assets/Scripts/MovementAnimator.cs
@@ -0,0 +1,39 @@
+﻿using StarterAssets;
+using UnityEngine;
+
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 移動動畫
+    /// </summary>
+    public class MovementAnimator : MonoBehaviour
+    {
+        [SerializeField, Header("角色控制器")]
+        private CharacterController characterController;
+        [SerializeField, Header("第一人稱控制器")]
+        private FirstPersonController firstPersonController;
+
+        private Animator ani;
+        private string parMove = "移動";
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        /// <summary>
+        /// 更新動畫
+        /// </summary>
+        private void UpdateAnimator()
+        {
+            ani.SetFloat(parMove, characterController.velocity.magnitude / firstPersonController.SprintSpeed);
+        }
+    }
+}
+
